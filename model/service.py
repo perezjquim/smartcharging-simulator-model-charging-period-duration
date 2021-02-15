fd1efc0c67baa01817a493471a977f066fe5d382
Joaquim Perez <jquim1@hotmail.com>
2021-02-15 21:21:09 +0000

    cleanup

--- model/service.py
+++ model/service.py
@@ -12,20 +12,20 @@ class ModelService:
     CHARGING_PERIOD_DURATION_AVG = 142.889
     CHARGING_PERIOD_DURATION_STDDEV = 44.898
 
-    name = 'model_charging_period_duration'
+    name = 'model_energysim_charging_period_duration'
 
-    event_dispatcher = EventDispatcher()
+    event_dispatcher = EventDispatcher( )
 
     @rpc
-    def get_charging_period_duration(self):
-        charging_period_duration = self.generate_charging_period_duration()
-        response = json.dumps({'charging_period_duration': charging_period_duration})
+    def get_duration( self ):
+        charging_period_duration = self.generate_duration( )
+        response = json.dumps( { 'charging_period_duration': charging_period_duration } )
         return response
 
-    def generate_charging_period_duration(self):
-        shape = [1,1]
-        min_charging_period_duration = self.CHARGING_PERIOD_DURATION_AVG - self.CHARGING_PERIOD_DURATION_STDDEV
-        max_charging_period_duration = self.CHARGING_PERIOD_DURATION_AVG + self.CHARGING_PERIOD_DURATION_STDDEV
+    def generate_duration( self ):
+        shape = [ 1,1 ]
+        min_charging_period_duration = ModelService.CHARGING_PERIOD_DURATION_AVG - ModelService.CHARGING_PERIOD_DURATION_STDDEV
+        max_charging_period_duration = ModelService.CHARGING_PERIOD_DURATION_AVG + ModelService.CHARGING_PERIOD_DURATION_STDDEV
 
         tf_random = tf.random.uniform(
                 shape=shape,
@@ -37,11 +37,11 @@ class ModelService:
         )
         tf_var = tf.Variable( tf_random )
 
-        tf_init = tf.compat.v1.global_variables_initializer()
-        tf_session = tf.compat.v1.Session()
-        tf_session.run(tf_init)
+        tf_init = tf.compat.v1.global_variables_initializer( )
+        tf_session = tf.compat.v1.Session( )
+        tf_session.run( tf_init )
 
-        tf_return = tf_session.run(tf_var)
+        tf_return = tf_session.run( tf_var )
         charging_period_duration = float( tf_return[ 0 ][ 0 ] )
 
         return charging_period_duration
